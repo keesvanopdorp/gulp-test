7fb8f65137fe2fab60e6fbe073befd48ffaae011
Steelshadows <its.a.me.brand@gmail.com>
2020-09-24 10:16:32 +0200

    tests

--- assets/ts/index.ts
+++ assets/ts/index.ts
@@ -1,4 +1,8 @@
 const navLinkElements: HTMLAnchorElement[] = Array.from(document.querySelectorAll('.nav-link'));
+const test = 'kees';
+
+// tuple array (array met fixed size)
+const test2: [number, string, boolean] = [3, 'test', false];
 
 navLinkElements.forEach(navLinkElement => {
 	navLinkElement.addEventListener('click', (e: MouseEvent) => console.log(e));
@@ -8,3 +12,16 @@ const numberToAccountingString = (number: number): string | undefined => number 
 
 console.log(numberToAccountingString(-5));
 console.log(numberToAccountingString(5));
+
+interface User {
+	id: number;
+	name: string;
+}
+
+const user: User = {id: 1, name: 'kees'};
+
+const testFunction = (type: 'kees' | 'brand') => {
+	console.log(type);
+};
+
+testFunction('kees');
---
+++ dist/js/index.js
@@ -0,0 +1,16 @@
+"use strict";
+const navLinkElements = Array.from(document.querySelectorAll('.nav-link'));
+const test = 'kees';
+// tuple array (array met fixed size)
+const test2 = [3, 'test', false];
+navLinkElements.forEach(navLinkElement => {
+    navLinkElement.addEventListener('click', (e) => console.log(e));
+});
+const numberToAccountingString = (number) => number !== null ? number < 0 ? `(${Math.abs(number)})` : number.toString() : undefined;
+console.log(numberToAccountingString(-5));
+console.log(numberToAccountingString(5));
+const user = { id: 1, name: 'kees' };
+const testFunction = (type) => {
+    console.log(type);
+};
+testFunction('kees');
